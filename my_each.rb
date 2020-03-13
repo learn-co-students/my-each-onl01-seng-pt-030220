ec1abb132db11a0db6667c52f3fb7ea50e24b1f8
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-03-13 14:07:00 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+collection = [1, 2, 3, 4]
+
+def my_each(array)
+  i=0 
+  while i < array.length 
+    yield(array[i])
+    i += 1 
+  end 
+  array 
 end
