d7d6c0bdb38ffc59f379a46dbd2a5d5718cc280e
Benjamin A Ajisefinni Jr <aj.no.limit.1@gmail.com>
2020-03-14 05:23:42 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each (array) 
+  
+  i = 0
+  array1 = []
+  
+  while i < array.length
+    array1 << yield(array[i])
+    i = i + 1
+  end
+  
+  array
+  
 end
